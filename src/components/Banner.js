6d52e621d03b851831bcf793dfea8dc97fc061e4
SuperSk <samarashu02@gmail.com>
2021-11-03 22:01:54 +0530

    or replace with and in banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,7 +5,7 @@ export const Banner = () => {
         <section id="banner">
             <div className="inner">
                 <h2>Hi, I am Samarth.</h2>
-                <p>I am an Android Developer.<br/> I build Android Apps using Kotlin and Backend using Ktor or Node.js</p>
+                <p>I am an Android Developer.<br/> I build Android Apps using Kotlin and Backend using Ktor and Node.js</p>
             </div>
         </section>
     )
