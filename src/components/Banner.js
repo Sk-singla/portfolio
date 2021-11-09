59e8a197e965d033188487e14de614d76971a915
SuperSk <samarashu02@gmail.com>
2021-11-09 17:49:26 +0530

    fab changed to fa

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -10,10 +10,10 @@ export const Banner = () => {
                 </p>
                 <br/>
                 <a href="https://www.linkedin.com/in/samarth-gupta-049120193" target="_blank" rel="noreferrer">
-                    <i className="fab fa-linkedin me-3" style={{fontSize:"24px"}}/>
+                    <i className="fa fa-linkedin me-3" style={{fontSize:"24px"}}/>
                 </a>
                 <a href="https://www.instagram.com/sk_singla02" target="_blank" rel="noreferrer">
-                    <i className="fab fa-instagram me-3" style={{fontSize: "24px"}}/>
+                    <i className="fa fa-instagram me-3" style={{fontSize: "24px"}}/>
                 </a>
             </div>
         </section>
