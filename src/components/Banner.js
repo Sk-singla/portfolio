1ac9e1e4976ea86fa55efbf71cdb19abd98e0b2e
Samar-Lpu-sk <samarth.11911223@lpu.in>
2021-12-27 17:33:19 +0530

    github link added

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -9,11 +9,14 @@ export const Banner = () => {
                     I build Android Apps using Kotlin and Backend using Ktor and Node.js
                 </p>
                 <br/>
-                <a href="https://www.linkedin.com/in/samarth-gupta-049120193" target="_blank" rel="noreferrer">
+                <a href="https://www.linkedin.com/in/samarth-gupta-049120193/" target="_blank" rel="noreferrer">
                     <i className="fa fa-linkedin me-3" style={{fontSize:"24px"}}/>
                 </a>
                 <a href="https://www.instagram.com/sk_singla02/" target="_blank" rel="noreferrer">
                     <i className="fa fa-instagram me-3" style={{fontSize: "24px"}}/>
+                </a>
+                <a href="https://github.com/Sk-singla/" target="_blank" rel="noreferrer">
+                    <i className="fa fa-github me-3" style={{fontSize: "24px"}}/>
                 </a>
             </div>
         </section>
